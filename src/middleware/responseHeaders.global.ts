b79e239f7cae1fdd25fe04c726dbefbe77ff09b3
Ivan Ryan <contact@ivanryan.slmail.me>
2023-02-19 14:59:13 -0600

    fix: setHeader

--- src/middleware/responseHeaders.global.ts
+++ src/middleware/responseHeaders.global.ts
@@ -1,13 +1,16 @@
 import { Middleware } from '@nuxt/types';
 
-const responseHeadersMiddleware: Middleware = (context: { res: { setHeader: (arg0: string, arg1: string) => void; }; }) => {
-  context.res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-  context.res.setHeader('X-XSS-Protection', '1; mode=block');
-  context.res.setHeader('X-Content-Type-Options', 'nosniff');
-  context.res.setHeader('Content-Security-Policy', 'upgrade-insecure-requests; block-all-mixed-content;');
-  context.res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-  context.res.setHeader('Feature-Policy', "camera 'none'; microphone 'none'");
-  context.res.setHeader('Permissions-Policy', 'camera=(), microphone=()');
+const responseHeadersMiddleware: Middleware = (context) => {
+  const { res } = context;
+  if (res) {
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('Content-Security-Policy', 'upgrade-insecure-requests; block-all-mixed-content;');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    res.setHeader('Feature-Policy', "camera 'none'; microphone 'none'");
+    res.setHeader('Permissions-Policy', 'camera=(), microphone=()');
+  }
 };
 
 export default responseHeadersMiddleware;
